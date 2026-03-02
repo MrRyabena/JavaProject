56e41a319f99079fbf65f069538f2a5a17477e95
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2026-03-02 22:43:12 +0300

    Created main file.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import dragon.*;
+import java.util.HashSet;
+import collection.Collection;
+
+public class Main {
+
+    public static void main(String args[]) {
+        var collection = new HashSet<Dragon>(32);
+            
+        }
+
+}
