700f43abab360d2a90fd038716ed45a796f6100e
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2026-03-04 12:25:33 +0300

    Testing.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,38 @@
 import core.Coordinates;
+
 import dragon.*;
-import java.util.HashSet;
+
 import storage.FileStorage;
+
+import java.util.HashSet;
 
 public class Main {
 
     public static void main(String args[]) {
-        var collection = new HashSet<Dragon>(32);
-        collection.add(new Dragon("Smaug", new Coordinates(10.0f, 20.0f), 500, 10000, DragonType.FIRE, new DragonHead(2.0f, 5.0f)));
-        collection.add(new Dragon("Charlie", new Coordinates(10.0f, 20.0f), 100, 10000, DragonType.FIRE, new DragonHead(2.0f, 5.0f)));   
-        
-        var storage = new FileStorage(java.nio.file.FileSystems.getDefault().getPath("data.json"), collection);
+        var collection = new collection.Collection();
+
+        collection.add(
+                new Dragon(
+                        "Smaug",
+                        new Coordinates(10.0f, 20.0f),
+                        500,
+                        10000,
+                        DragonType.FIRE,
+                        new DragonHead(2.0f, 5.0f)));
+        collection.add(
+                new Dragon(
+                        "Charlie",
+                        new Coordinates(10.0f, 20.0f),
+                        100,
+                        10000,
+                        DragonType.FIRE,
+                        new DragonHead(2.0f, 5.0f)));
+
+        var storage = new FileStorage(java.nio.file.FileSystems.getDefault().getPath(System.getenv("STORAGE_PATH")));
         try {
             storage.save(collection);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        }
-
+    }
 }
